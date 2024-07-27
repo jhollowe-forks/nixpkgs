3e8dafda424a85bd7e179ca0be9739807a7af3e0
John Hollowell <johnahollowell@gmail.com>
2024-07-27 23:58:03 +0000

    WIP?

---
+++ pkgs/by-name/sh/shinysdr/package.nix
@@ -0,0 +1,38 @@
+{ lib
+, gnuradio
+, fetchFromGitHub
+}:
+
+gnuradio.pkgs.mkDerivation rec {
+  pname = "shinysdr";
+  version = "0.3.1";
+
+  src = fetchFromGitHub {
+    owner = "miek";
+    repo = "inspectrum";
+    rev = "v${version}";
+    sha256 = "sha256-yY2W2hQpj8TIxiQBSbQHq0J16n74OfIwMDxFt3mLZYc=";
+  };
+
+  nativeBuildInputs = [
+    cmake
+    qt5.wrapQtAppsHook
+    pkg-config
+  ];
+  buildInputs = [
+    fftwFloat
+    liquid-dsp
+    qt5.qtbase
+  ] ++ lib.optionals (gnuradio3_8Minimal.hasFeature "gr-ctrlport") [
+    thrift
+    gnuradio3_8Minimal.unwrapped.python.pkgs.thrift
+  ];
+
+  meta = with lib; {
+    description = "Tool for analysing captured signals from sdr receivers";
+    homepage = "https://github.com/miek/inspectrum";
+    maintainers = with maintainers; [ mog ];
+    platforms = platforms.linux;
+    license = licenses.gpl3Plus;
+  };
+}
